e3eaef474ef5e6328692ac9af1137ec10a589000
Luis Sanchez <luis.udec.sanchez@hotmail.com>
2024-01-28 15:41:33 -0500

    fix: "Update API_URL"

--- client/src/utils/fetchRepos.ts
+++ client/src/utils/fetchRepos.ts
@@ -2,7 +2,7 @@ import { UserRepositories } from "../types";
 
 export const fetchRepos = async (pathName: string): Promise<UserRepositories> => {
   try {
-    const url = new URL(pathName, "http://localhost:3000/");
+    const url = new URL(pathName, "https://technical-test-backend-6vi1.onrender.com/");
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('Error fetching repositories');
